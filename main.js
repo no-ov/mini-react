fd1a5be7f3f0a8df191d1f68f8345d4c4374c664
没有达布妞 <qixq@outlook.com>
2024-01-13 20:04:49 +0800

    feat: step2. 创建vdom 对象

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
 // v1 
-const dom = document.createElement('div')
-dom.id = 'app'
+// const dom = document.createElement('div')
+// dom.id = 'app'
+// document.querySelector('#root').append(dom)
+
+
+// const textNode = document.createTextNode("")
+// textNode.nodeValue = 'app'
+// dom.append(textNode)
+
+
+// v2 react -> vdom -> js object
+
+// 创建 虚拟dom 对象
+
+// type props children
+
+const textEl = {
+  type: 'div',
+  props: {
+    id: 'app',
+    children: {
+      type: 'TEXT_ELEMENT',
+      props: {
+        nodeValue: 'app',
+        children: []
+      }
+    }
+  }
+}
+
+const el = {
+  type: 'div',
+  props: {
+    id: 'app',
+    children: [textEl]
+  }
+}
+
+
+const dom = document.createElement(el.type)
+dom.id = el.props.id
 document.querySelector('#root').append(dom)
 
 
 const textNode = document.createTextNode("")
-textNode.nodeValue = 'app'
+textNode.nodeValue = textEl.props.nodeValue
 dom.append(textNode)
-
-
